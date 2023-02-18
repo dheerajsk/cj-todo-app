384d6092fae1b28eb36dc86da353ef1b302f257f
dheerajsk <dheeraj.s.roy@gmail.com>
2023-02-18 11:52:18 +0530

    note reducer

--- src/components/NoteList/NoteList.js
+++ src/components/NoteList/NoteList.js
@@ -1,11 +1,7 @@
 import "./NoteList.css";
 
 function NoteList() {
-    const notes=[{text:'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam'
-    , createdOn: new Date()},
-    {text:'Aliquam erat volutpat. Ut tincidunt, velit vel aliquam commodo, tellus urna auctor tortor, non ultrices libero ante sed magna.'
-    , createdOn: new Date()},
-];
+    const notes=[];
   return (
     <div className="container">
     <ul>
